18170f3d803405a68b30d2c3c14ba19ea5e883dd
Satriyo Rizkyansah <satttrioo@gmail.Com>
2025-03-29 11:10:14 +0700

    FlatList

--- App.js
+++ App.js
@@ -10,15 +10,45 @@ import {
   View,
   Button,
   Alert,
+  FlatList,
 } from 'react-native';
 
 const App = () => {
+  const negara = [
+    'indonesia',
+    'jepang',
+    'amerika',
+    'inggris',
+    'cina',
+    'afrika',
+    'thailand',
+    'austraia',
+  ];
+
   return (
     <View style={{marginTop: 100}}>
-      <Button
-        title="Login"
-        color={'#212121'}
-        onPress={() => Alert.alert('Hello World')}
+      <StatusBar backgroundColor={'#21252b'} barStyle={'light-content'} />
+      <FlatList
+        data={negara}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item, index}) => (
+          <View
+            style={{
+              marginHorizontal: 20,
+              marginTop: 10,
+              backgroundColor: '#282c34',
+              paddingTop: 100,
+              paddingBottom: 100,
+              justifyContent: 'center',
+              borderRadius: 4,
+              alignItems: 'center',
+              elevation: 3,
+              marginBottom: 10,
+              // paddingHorizontal: 100,
+            }}>
+            <Text style={{color: '#fff'}}>{item}</Text>
+          </View>
+        )}
       />
     </View>
   );
