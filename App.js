ee58a1afaf47425ebc6d464306614639955f4301
Satriyo Rizkyansah <satttrioo@gmail.Com>
2025-03-30 11:03:21 +0700

    FlatList Array of Objects

--- App.js
+++ App.js
@@ -25,11 +25,52 @@ const App = () => {
     'austraia',
   ];
 
+  const daftarNegara = [
+    {
+      nama: 'Indonesia',
+      deskripsi: 'ini negara indonesia',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/0/0b/Flag_of_Indonesia.png',
+    },
+    {
+      nama: 'Jepang',
+      deskripsi: 'ini negara Jepang',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtuTcc7-loP1DSWXEPOu74mwIfey6iTkEApw&s',
+    },
+  ];
+
   return (
+    // <View style={{marginTop: 100}}>
+    //   <StatusBar backgroundColor={'#21252b'} barStyle={'light-content'} />
+    //   <FlatList
+    //     data={negara}
+    //     keyExtractor={(item, index) => index.toString()}
+    //     renderItem={({item, index}) => (
+    //       <View
+    //         style={{
+    //           marginHorizontal: 20,
+    //           marginTop: 10,
+    //           backgroundColor: '#282c34',
+    //           paddingTop: 100,
+    //           paddingBottom: 100,
+    //           justifyContent: 'center',
+    //           borderRadius: 4,
+    //           alignItems: 'center',
+    //           elevation: 3,
+    //           marginBottom: 10,
+    //           // paddingHorizontal: 100,
+    //         }}>
+    //         <Text style={{color: '#fff'}}>{item}</Text>
+    //       </View>
+    //     )}
+    //   />
+    // </View>
+
     <View style={{marginTop: 100}}>
       <StatusBar backgroundColor={'#21252b'} barStyle={'light-content'} />
       <FlatList
-        data={negara}
+        data={daftarNegara}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({item, index}) => (
           <View
@@ -37,16 +78,27 @@ const App = () => {
               marginHorizontal: 20,
               marginTop: 10,
               backgroundColor: '#282c34',
-              paddingTop: 100,
-              paddingBottom: 100,
-              justifyContent: 'center',
+              paddingTop: 50,
+              paddingBottom: 50,
+              // justifyContent: 'center',
               borderRadius: 4,
               alignItems: 'center',
               elevation: 3,
               marginBottom: 10,
               // paddingHorizontal: 100,
+              flexDirection: 'row',
+              borderRadius: 15,
             }}>
-            <Text style={{color: '#fff'}}>{item}</Text>
+            <Image
+              source={{uri: item.image}}
+              style={{width: 75, height: 50, borderRadius: 5, marginLeft: 20}}
+            />
+            <View style={{marginLeft: 10}}>
+              <Text style={{color: '#fff', fontWeight: 'bold'}}>
+                {item.nama}
+              </Text>
+              <Text style={{color: '#fff'}}>{item.deskripsi}</Text>
+            </View>
           </View>
         )}
       />
