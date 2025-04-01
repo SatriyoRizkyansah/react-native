69f0c58158f3bcf1cf72852fbeb79ca2fffb3f10
Satriyo Rizkyansah <satttrioo@gmail.Com>
2025-04-01 15:49:46 +0700

    Linking

--- App.js
+++ App.js
@@ -1,26 +1,34 @@
 import {useEffect, useState} from 'react';
-import {ImageBackground, Text, View} from 'react-native';
+import {
+  ImageBackground,
+  Text,
+  View,
+  Linking,
+  TouchableOpacity,
+} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{marginTop: 100}}>
-      <ImageBackground
-        source={require('./src/images/kucing.jpg')}
-        style={{
-          marginHorizontal: 20,
-          marginTop: 10,
-        }}
-        imageStyle={{borderRadius: 9}}>
-        <View style={{backgroundColor: 'rgba(0,0,0,0.5)', padding: 15}}>
-          <Text style={{color: 'white'}}>Image Background</Text>
-          <Text style={{color: 'white'}}>Image Background</Text>
-          <Text style={{color: 'white'}}>Image Background</Text>
-          <Text style={{color: 'white'}}>Image Background</Text>
-          <Text style={{color: 'white'}}>Image Background</Text>
-          <Text style={{color: 'white'}}>Image Background</Text>
-        </View>
-      </ImageBackground>
-    </View>
+    <TouchableOpacity onPress={() => Linking.openURL('https://google.com')}>
+      <View style={{marginTop: 100}}>
+        <ImageBackground
+          source={require('./src/images/kucing.jpg')}
+          style={{
+            marginHorizontal: 20,
+            marginTop: 10,
+          }}
+          imageStyle={{borderRadius: 9}}>
+          <View style={{backgroundColor: 'rgba(0,0,0,0.5)', padding: 15}}>
+            <Text style={{color: 'white'}}>Image Background</Text>
+            <Text style={{color: 'white'}}>Image Background</Text>
+            <Text style={{color: 'white'}}>Image Background</Text>
+            <Text style={{color: 'white'}}>Image Background</Text>
+            <Text style={{color: 'white'}}>Image Background</Text>
+            <Text style={{color: 'white'}}>Image Background</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    </TouchableOpacity>
   );
 };
 
